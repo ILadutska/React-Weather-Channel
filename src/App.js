c3d28524dc36b5414bca07f8fefcf67f34fc8c3c
ILadutska <ivan.ladutska@gmail.com>
2023-11-06 07:56:29 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import logo from './logo.png';
+import kramer from './kramer-worship.gif'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="image-container">
+          <img src={logo} className="cw-logo" alt="logo" />
+          <img src={logo} className="fast-ccw-logo" alt="logo" />
+          <img src={logo} className="fast-cw-logo" alt="logo" />
+          <img src={logo} className="ccw-logo" alt="logo" />
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Cool Stuff
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.weather.gov/"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <img src={kramer} className="App-kramer" alt="kramer" />
       </header>
     </div>
   );
