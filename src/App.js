65d573317df6a015e584a26a4517c16bcc7febbd
FaizaJalees <fjalees01@student.ysu.edu>
2023-11-06 20:50:37 -0500

    added current day, date, time

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
+import React from 'react';
 import logo from './logodesigns.png';
-import background from './mountains.jpg'
+import background from './mountains.jpg';
 import './App.css';
+import DateTime from './DateTime.js';
 
 function App() {
   return (
     <div className="App">
         <div className="image-container">
+        <div className = "DateTimeDay"><DateTime></DateTime></div>
         {<img src={background} className="background" alt="mountains"/>}
+        
         {<img src={logo} className="cw-logo" alt="logo" />}
+        
         </div>
-        <div class="search-container">
+        <div className="search-container">
         <input type="text" placeholder="Search..."></input>
-        <button type="submit">Search</button>
-    </div>
-    <div class="containerTime">
-        <h6 id="currentTime"></h6>
-    </div>
-    <div class="containerDay">
-        <h6 id="currentDay"></h6>
-    </div>
-    <div class="containerDate">
-        <h6 id="currentDate"></h6>
-    </div>
-    <script src="script.js"></script>
-          
-
-        {/* <p>
-          Cool Stuff
-        </p>
-        <a
-          className="App-link"
-          href="https://www.weather.gov/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <button type="submit">Search</button></div>
+        
+        
     </div>
   );
 }
