c874697c61027bf61e89d348b65262af0cca5fbc
ILadutska <ivan.ladutska@gmail.com>
2023-11-12 11:27:34 -0500

    added router dom to package.json

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,11 +2,24 @@ import React from "react"
 import Navbar from '../components/Navbar'
 
 function AboutPage() {
+
+	const AboutStyle = {
+		backgroundSize: 'cover',
+		backgroundRepeat: 'no-repeat',
+		height: '100vh',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: "grey"
+	};
+
 	return (
-		<div>
+
+		<div style={AboutStyle}>
 			<div>
 				<Navbar></Navbar>
-				<div>About</div>
+				<div className="logo">About</div>
 			</div>
 		</div>
 	)
