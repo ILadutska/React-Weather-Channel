7690f410eb9846b487821c39af26269786730c92
nikolina-drobnjak <niki.drobnjak@gmail.com>
2023-11-20 19:58:27 -0500

    Display current weather data (hour 0).

--- src/components/Navbar-hours.js
+++ src/components/Navbar-hours.js
@@ -7,11 +7,11 @@ function NavbarHours({onHourSelect}) {
 	return (
 		<div className="navbar-hours">
 			<ul className="navbar-hours">
+				<li><a href="#" onClick={() => onHourSelect(0)}>Now</a></li>
 				<li><a href="#" onClick={() => onHourSelect(6)}>6 hours</a></li>
 				<li><a href="#" onClick={() => onHourSelect(12)}>12 hours</a></li>
 				<li><a href="#" onClick={() => onHourSelect(18)}>18 hours</a></li>
 				<li><a href="#" onClick={() => onHourSelect(24)}>24 hours</a></li>
-				<li><a href="#" onClick={() => onHourSelect(155)}>155 hours</a></li>
 			</ul>
 		</div>
 	)
