0c5d8b74c5e10c80eaf69090cdcf23ba881731a6
FatimaWasim <fwasim@student.ysu.edu>
2023-11-25 22:11:44 -0500

    Added elements needed on the daily advisory page

--- src/pages/DailyAdvisory.js
+++ src/pages/DailyAdvisory.js
@@ -1,10 +1,12 @@
-import React, { useContext } from 'react';
-import { AppContext, AppProvider } from '../AppContext';
+import React, { useContext, useEffect, useState } from 'react';
+import { AppContext, AppProvider } from '../AppContext.js';
 import logo from '../resources/logodesigns.png';
-import background from '../resources/daily_advisory_bg.jpg';
+import background from '../resources/Daily_Advisory_new.jpg';
+import sunny from '../resources/sunny.png';
 import '../App.css';
 import DateTime from '../DateTime.js';
 import Navbar from '../components/Navbar-pages.js';
+import NavbarDays from '../components/Navbar-days.js';
 
 function DailyAdvisory() {
 
@@ -15,8 +17,25 @@ function DailyAdvisory() {
 		latitude,
 		longitude,
 		onOptionSelect,
-		options
+		options,
+		hourlyData,
+		dailyData,
+		city,
+		alertData
 	} = useContext(AppContext);
+
+	const [selectedHour, setSelectedHour] = useState('');
+	const [selectedDay, setSelectedDay] = useState('');
+
+
+
+
+	const handleDaySelection = (numVal) => {
+		setSelectedHour('');
+		console.log('Selected Day:', numVal);
+		setSelectedDay(numVal);
+		console.log('Updated: ', selectedDay);
+	};
 
 	const homeStyle = {
 		backgroundImage: `url(${background})`,
@@ -33,44 +52,92 @@ function DailyAdvisory() {
 	return (
 		<div style={homeStyle}>
 			<Navbar></Navbar>
-			<div className="App">
+			<NavbarDays onDaySelect={handleDaySelection} />
 
-				<div className="search-container">
-					<div className="search-input">
-						<input
-							type="text"
-							placeholder="Search..."
-							onChange={onInputChange}
-							value={userInput}
-							onKeyPress={(e) => {
-								if (e.key === 'Enter') {
-									fetchWeatherData(latitude, longitude);
-								}
-							}}
-						/>
-						<button type="submit" onClick={() => onOptionSelect(options)}>
-							Search
-						</button>
-					</div>
-					<ul className="options-list">
-						{options.map((option, index) => (
-							<li key={option.name + '-' + index}>
-								<button onClick={() => onOptionSelect(option)}>
-									{option.name + ', ' + option.state + ', ' + option.country}
-								</button>
-							</li>
-						))}
-					</ul>
+			<div className="search-container">
+				<div className="search-input">
+					<input
+						type="text"
+						placeholder="Search..."
+						onChange={onInputChange}
+						value={userInput}
+						onKeyPress={(e) => {
+							if (e.key === 'Enter') {
+								fetchWeatherData(latitude, longitude);
+							}
+						}}
+					/>
+					<button type="submit" onClick={() => onOptionSelect(options)}>
+						Search
+					</button>
 				</div>
-
-				<div className="image-container">
-					<div className="DateTimeDay"><DateTime></DateTime></div>
-					{<img className="background" />}
-					{<img src={logo} className="logo" alt="logo" />}
-
-				</div>
+				<ul className="options-list">
+					{options.map((option, index) => (
+						<li key={option.name + '-' + index}>
+							<button onClick={() => onOptionSelect(option)}>
+								{option.name + ', ' + option.state + ', ' + option.country}
+							</button>
+						</li>
+					))}
+				</ul>
 			</div>
+
+
+			<div className="image-container">
+				<div className="DateTimeDay">
+					<DateTime></DateTime>
+				</div>
+				{<img src={logo} className="logo" alt="logo" />}
+
+			</div>
+			{selectedDay !== '' && dailyData.periods[selectedDay].shortForecast === "Sunny" ? (
+				console.log("the conditional worked"),
+				<img src={sunny} alt="Sunny" className="sunny" />
+			) : (
+				null
+			)}
+
+			<div>
+				{dailyData && dailyData.periods && dailyData.periods.length > 1 ? (
+					<div className="weather-info-container">
+						<div className="weather-box">
+							<p>Latitude: {latitude}</p>
+						</div>
+						<div className="weather-box">
+							<p>Longitude: {longitude}</p>
+						</div>
+						<div className="weather-box">
+							<p>City: {city}</p>
+						</div>
+						{selectedDay !== '' ? (
+							<>
+								<div className="weather-box">
+									<p>Day: {dailyData.periods[selectedDay].name}</p>
+								</div>
+								<div className="weather-box">
+									<p>Temperature: {dailyData.periods[selectedDay].temperature}°F</p>
+								</div>
+								<div className="weather-box">
+									<p>Event: {alertData.event}</p>
+								</div>
+
+								<div className="weather-box">
+									<p>As part of our commitment to keeping you informed about weather conditions,</p>
+									<p> we want to bring your attention to today's weather advisory.</p>
+								</div>
+
+
+							</>
+						) : null}
+
+					</div>
+				) : (
+					<p>Loading...</p>
+				)}
+			</div>
+
 		</div>
+
 	);
 }
 
